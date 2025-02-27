293a44ce921afbb1c60603f3e27b423af2bc2ac0
22420kv <22420kv@hvhs.school.nz>
2025-02-27 22:00:34 +0000

    update 5

--- GameThatWorks.js
+++ GameThatWorks.js
@@ -9,11 +9,11 @@
 /*******************************************************/
 var score = 0;
 var player;
-const PLAYERSIZE = 20;
-const MOVEMENTSPEED = 5;
+const PLAYERSIZE = 40;
+const MOVEMENTSPEED = 10;
 
-const GAMEHEIGHT = 1850;
-const GAMEWIDTH = 860;
+//const GAMEHEIGHT = 1850;
+//const GAMEWIDTH = 860;
 
 const COINSIZE = 10;
 const COIN_TIMEOUT = 2000;
@@ -21,14 +21,25 @@ var coin;
 
 var gameState = "play";
 
+var cnv;
+
+
 /*******************************************************/
 // setup()
 /*******************************************************/
+function centerCanvas() {
+	var x = (windowWidth - width) / 2;
+	var y =(windowHeight - height) / 2;
+	cnv.position(x, y);
+}
+
 function setup() {
 	console.log("setup: ");
 
-	cnv = new Canvas(windowWidth, windowHeight);
-	player = new Sprite(900, 400, PLAYERSIZE);
+	cnv = createCanvas(800, 800);
+	centerCanvas();
+
+	player = new Sprite(400, 400, PLAYERSIZE);
 	player.color = 'orange';
     player.stroke = 'yellow';
 	player.strokeWeight = '5';
@@ -42,15 +53,12 @@ function setup() {
 		collider2.remove();
 		score++;
 	}
-}
-	
+}	
 
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	//background('grey');
-
 	if (gameState == "play") {
 		runGame();
 	} else if (gameState == "lose") {
@@ -60,13 +68,13 @@ function draw() {
 }	
 
 function runGame() {
-	background('grey');
-	if (random(0, 100)<5) {
+	background('darkblue');
+	if (random(0, 1000)<20) {
 		coins.add(createCoin());
 	}
 
 	movePlayer();
-	  for (var i = 0; i < coins.lenght; i++) {
+	  for (var i = 0; i < coins.length; i++) {
 		if(checkCoinTime(coins[i])) {
 			coins[i].remove();
 			gameState = "lose";
@@ -81,9 +89,11 @@ function loseScreen() {
 	text("You missed a coin! ", 10,100);
 	textSize(100);
 	text("Score: " + score, 10,200);
+	player.remove();
+	coins.remove();
 }
 
-function checkCountTime(_coin) {
+function checkCoinTime(_coin) {
 	if (_coin.spawntime + COIN_TIMEOUT < millis()) {
 		return(true);
 	}
@@ -91,7 +101,7 @@ function checkCountTime(_coin) {
 }
 
 function createCoin() {
-    var coin = new Sprite(random(0, GAMEHEIGHT), random(0, GAMEWIDTH), COINSIZE);
+    var coin = new Sprite(random(0, 800), random(0, 800), COINSIZE);
     coin.color = 'lightgrey';
 	coin.spawntime = millis();
 	  return(coin);
@@ -104,25 +114,25 @@ function displayScore() {
 
 function movePlayer() {
     if (kb.pressing('left')) {
-		player.vel.x = -5;  //Keyboard input D
+		player.vel.x = -MOVEMENTSPEED;  //Keyboard input D
 	}
 	if (kb.releases('left')) {
 		player.vel.x = 0;
 	}
 	if (kb.pressing('right')) {
-		player.vel.x = 5;  //Keyboard input A
+		player.vel.x = MOVEMENTSPEED;  //Keyboard input A
 	}
 	if (kb.releases('right')) {
 		player.vel.x = 0;
 	}
 	if (kb.pressing('up')) {
-		player.vel.y = -5;  //Keyboard input W
+		player.vel.y = -MOVEMENTSPEED;  //Keyboard input W
 	}
 	if (kb.releases('up')) {
 		player.vel.y = 0;
 	}
 	if (kb.pressing('down')) {
-		player.vel.y = 5;  //Keyboard input S
+		player.vel.y = MOVEMENTSPEED;  //Keyboard input S
 	}
 	if (kb.releases('down')) {
 		player.vel.y = 0;
